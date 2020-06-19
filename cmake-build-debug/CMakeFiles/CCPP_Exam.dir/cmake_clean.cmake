d06dcdfb6ecb0cd4c31792e5d7f324c87817eca7
NoriCode <norikitamo@web.de>
2020-06-19 11:08:28 +0200

    rules and project start in clion

---
+++ cmake-build-debug/CMakeFiles/CCPP_Exam.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CCPP_Exam"
+  "CCPP_Exam.pdb"
+  "CMakeFiles/CCPP_Exam.dir/main.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CCPP_Exam.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/CCPP_Exam.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for CCPP_Exam.
+# This may be replaced when dependencies are built.
