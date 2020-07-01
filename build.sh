30e3f01137fd88e3f0649254e1ef0cd134e2ff9a
Nori <nori@pop-os.localdomain>
2020-07-01 15:15:10 +0200

    Bugs Fixed and now tetsing

--- build.sh
+++ build.sh
@@ -2,14 +2,14 @@
 CPP_SRC_UTIL_PATH=${CPP_SRC_UTIL_PATH:-.}
 CPPFLAGS="-Wall -Wextra -Wpedantic -O0 -std=c++14"
 
-clang++ "${CPPFLAGS}" -I"${CPP_SRC_UTIL_PATH}" -o CCPP_Exam.out main.cpp
-clang++ "${CPPFLAGS}" -I"${CPP_SRC_UTIL_PATH}" -o CCPP_Exam.out bjCards.cpp
-clang++ "${CPPFLAGS}" -I"${CPP_SRC_UTIL_PATH}" -o CCPP_Exam.out bjDealer.cpp
-clang++ "${CPPFLAGS}" -I"${CPP_SRC_UTIL_PATH}" -o CCPP_Exam.out bjDeck.cpp
-clang++ "${CPPFLAGS}" -I"${CPP_SRC_UTIL_PATH}" -o CCPP_Exam.out bjHand.cpp
+clang++ "${CPPFLAGS}" -I"${CPP_SRC_UTIL_PATH}" -v CCPP_Exam.out main.cpp
+clang++ "${CPPFLAGS}" -I"${CPP_SRC_UTIL_PATH}" -v CCPP_Exam.out bjCards.cpp
+clang++ "${CPPFLAGS}" -I"${CPP_SRC_UTIL_PATH}" -v CCPP_Exam.out bjDealer.cpp
+clang++ "${CPPFLAGS}" -I"${CPP_SRC_UTIL_PATH}" -v CCPP_Exam.out bjDeck.cpp
+clang++ "${CPPFLAGS}" -I"${CPP_SRC_UTIL_PATH}" -v CCPP_Exam.out bjHand.cpp
 # shellcheck disable=SC2086
 # shellcheck disable=SC2086
-clang++ "${CPPFLAGS}" -I${CPP_SRC_UTIL_PATH} -o CCPP_Exam.out bjPlayer.cpp
+clang++ "${CPPFLAGS}" -I${CPP_SRC_UTIL_PATH} -v CCPP_Exam.out bjPlayer.cpp
 # shellcheck disable=SC2086
-clang++ "${CPPFLAGS}" -I${CPP_SRC_UTIL_PATH} -o CCPP_Exam.out bjRuleController.cpp
-clang++ "${CPPFLAGS}" -I"${CPP_SRC_UTIL_PATH}" -o CCPP_Exam.out blackJack.cpp
+clang++ "${CPPFLAGS}" -I${CPP_SRC_UTIL_PATH} -v CCPP_Exam.out bjRuleController.cpp
+clang++ "${CPPFLAGS}" -I"${CPP_SRC_UTIL_PATH}" -v CCPP_Exam.out blackJack.cpp
